af3fc2e6eb6067af6dc4f1ff567e373272a24b36
Harsh Patel <harshp1198@gmail.com>
2026-08-27 15:32:28 -0600

    Add Medium Python Solution for 424. Longest Repeating Character Replacement from LeetCode

--- LeetCode/Patterns/Sliding Window/424. Longest Repeating Character Replacement/Solution.py
+++ LeetCode/Patterns/Sliding Window/424. Longest Repeating Character Replacement/Solution.py
@@ -3,12 +3,16 @@ class Solution:
         count = {}
         max_len = 0
         l = 0
+        #maxf = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            
-            if (r - l + 1) - max(count.values()) > k:
+            #maxf = max(maxf, count[s[r]]) if use this, no need to scan whole hashmap
+            while (r - l + 1) - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
             max_len = max(max_len, r - l + 1)
         
         return max_len
+
+        # time: O(26*n)
+        # space: O(26)
