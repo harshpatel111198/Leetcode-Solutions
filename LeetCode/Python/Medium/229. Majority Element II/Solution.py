1451a6812b22c08b0364f294479fdc90dc8817d2
Harsh Patel <harshp1198@gmail.com>
2026-09-08 12:52:34 -0600

    Add Medium Python Solution for 229. Majority Element II from LeetCode

--- LeetCode/Python/Medium/229. Majority Element II/Solution.py
+++ LeetCode/Python/Medium/229. Majority Element II/Solution.py
@@ -1,15 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        frequency = {}
+        cnt1, cnt2 = 0, 0
+        el1 = float("-inf")
+        el2 = float("-inf")
+
+        for i in range(len(nums)):
+            if cnt1 == 0 and nums[i] != el2:
+                cnt1 = 1
+                el1 = nums[i]
+            elif cnt2 == 0 and nums[i] != el1:
+                cnt2 = 1
+                el2 = nums[i]
+            elif el1 == nums[i]:
+                cnt1 += 1
+            elif el2 == nums[i]:
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
         res = []
-        appearance = len(nums)//3 + 1
+        cnt1, cnt2 = 0, 0
         for n in nums:
-            frequency[n] = frequency.get(n, 0) + 1
-            
-            if frequency[n] == appearance:
-                res.append(n)
-            
-            if len(res) == 2:
-                break
-            
+            if n == el1:cnt1 += 1
+            if n == el2:cnt2 += 1
+        mini = (len(nums) // 3) + 1
+        if cnt1 >= mini:res.append(el1)
+        if cnt2 >= mini:res.append(el2)
         return res
