a59cb34c45eca26210d07663d294d85312740f6b
Harsh Patel <harshp1198@gmail.com>
2026-09-08 12:10:36 -0600

    Add Medium Python Solution for 229. Majority Element II from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/229. Majority Element II/Solution.py
+++ LeetCode/Patterns/Hash Map & Hashing/229. Majority Element II/Solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         frequency = {}
-        res = []
+        res = set()
+        appearance = len(nums)//3
         for n in nums:
             frequency[n] = frequency.get(n, 0) + 1
-        
-        appearance = len(nums)//3
-
-        for n in frequency:
+            
             if frequency[n] > appearance:
-                res.append(n)
-        return res
+                res.add(n)
+        return list(res)
