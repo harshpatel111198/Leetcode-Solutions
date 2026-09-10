863560b0f07382b0fcbe4a01dad45fbec939e9ac
Harsh Patel <harshp1198@gmail.com>
2026-09-10 18:52:36 -0400

    Add Medium Python Solution for 15. 3Sum from LeetCode

--- LeetCode/Python/Medium/15. 3Sum/Solution.py
+++ LeetCode/Python/Medium/15. 3Sum/Solution.py
@@ -1,13 +1,23 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        res = set()
+        res = []
+        nums.sort()
         for i in range(len(nums)):
-            hash_map = {}
-            for j in range(i+1,len(nums)):
-                k = -(nums[i] + nums[j]) 
-                if k in hash_map.values():
-                    tup = tuple(sorted([nums[i], nums[j], k]))
-                    res.add(tup)
-                hash_map[i+j] = nums[j]
-           
-        return list(res)
+            if i > 0 and nums[i] == nums[i-1]: continue
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j +=1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -=1
+        return res
