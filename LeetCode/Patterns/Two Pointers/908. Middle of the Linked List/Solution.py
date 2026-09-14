16a4d929276a78b8ebb739cba4e7a61f89adefc1
Harsh Patel <harshp1198@gmail.com>
2026-09-14 14:40:17 -0400

    Add Easy Python Solution for 908. Middle of the Linked List from LeetCode

--- LeetCode/Patterns/Two Pointers/908. Middle of the Linked List/Solution.py
+++ LeetCode/Patterns/Two Pointers/908. Middle of the Linked List/Solution.py
@@ -5,18 +5,23 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        count_node = 0
-        temp = head
-        while temp:
-            count_node += 1
-            temp = temp.next
-        
-        middle_count = count_node // 2
-        print(middle_count, count_node)
-        count_node = 0
-        temp2 = head
-        while temp2:
-            if count_node == middle_count:
-                return temp2
-            temp2 = temp2.next
-            count_node += 1
+  
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        #   count_node = 0
+        # temp = head
+        # while temp:
+        #     count_node += 1
+        #     temp = temp.next
+        # middle_count = count_node // 2
+        # count_node = 0
+        # temp2 = head
+        # while temp2:
+        #     if count_node == middle_count:
+        #         return temp2
+        #     temp2 = temp2.next
+        #     count_node += 1
