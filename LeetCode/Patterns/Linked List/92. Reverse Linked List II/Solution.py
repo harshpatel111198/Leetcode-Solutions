8457972eec98985aeb4668ecbd92dc80bafa48e0
Harsh Patel <harshp1198@gmail.com>
2026-09-14 16:10:40 -0400

    Add Medium Python Solution for 92. Reverse Linked List II from LeetCode

--- LeetCode/Patterns/Linked List/92. Reverse Linked List II/Solution.py
+++ LeetCode/Patterns/Linked List/92. Reverse Linked List II/Solution.py
@@ -5,25 +5,27 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if right == left:
+            return head
         temp = head
         count = 0
         prev = temp
         temp2 = None
         node = None
-        while temp.next:
+        while temp:
             count += 1
-            temp2 = temp.next
+            temp2 = temp
 
             if count == left:
-                while temp2 and count <= right:
+                while temp2 and count <= right and left != right:
                     t = temp2.next
                     temp2.next = node
                     node = temp2
                     temp2 = t
                     count += 1  
-            prev.next = node
+                prev.next = node
             temp = temp.next
-            break
+        print(prev, node)
         head = prev
         while prev.next is not None:
             prev = prev.next
