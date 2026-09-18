d2826abec9e07731e287045e5461131f13c3432f
Harsh Patel <harshp1198@gmail.com>
2026-09-18 14:43:22 -0600

    Add Medium Python Solution for 2095. Delete the Middle Node of a Linked List from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/2095. Delete the Middle Node of a Linked List/Solution.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: ListNode | None) -> ListNode | None:
+        slow = fast = head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        if prev and prev.next and prev.next.next:
+            prev.next = prev.next.next
+        else:
+            prev.next = None
+        return head
