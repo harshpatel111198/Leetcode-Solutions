b9c8eb450caeacd7ef12e26c8a5d66ad7bacd916
Harsh Patel <harshp1198@gmail.com>
2026-09-18 14:25:09 -0600

    Add Medium Python Solution for 19. Remove Nth Node From End of List from LeetCode

--- LeetCode/Patterns/Two Pointers/19. Remove Nth Node From End of List/Solution.py
+++ LeetCode/Patterns/Two Pointers/19. Remove Nth Node From End of List/Solution.py
@@ -5,22 +5,40 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode | None, n: int) -> ListNode | None:
-        length = 1
-        curr = head
-        while curr.next:
-            curr = curr.next 
-            length += 1
+        # length = 1
+        # curr = head
+        # while curr.next:
+        #     curr = curr.next 
+        #     length += 1
         
-        if length == 1 and n == 1:
-            return None
-        if length == n:
+        # if length == 1:
+        #     return None
+        # if length == n:
+        #     return head.next
+        # n_node = 0
+        # curr = head
+        # while curr:
+        #     n_node += 1
+        #     if length - n_node == n:
+        #         curr.next = curr.next.next
+        #         break
+        #     curr = curr.next
+        # return head
+        # handle single node
+        if not head.next: return None
+        
+        ptr = temp = head
+        for _ in range(n):
+            ptr = ptr.next
+        # if ptr is null then removes the head node
+        if not ptr:
             return head.next
-        n_node = 0
-        curr = head
-        while curr:
-            n_node += 1
-            if length - n_node == n:
-                curr.next = curr.next.next
-                break
-            curr = curr.next
+        # move both until ptr reaches end
+        while ptr.next:
+            ptr = ptr.next
+            temp = temp.next
+        
+        
+        temp.next = temp.next.next
+
         return head
